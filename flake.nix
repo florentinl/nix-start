4e04ac41181d00c43e1cd8570fc12df8054e7fbd
Florentin Labelle <101594032+florentinl@users.noreply.github.com>
2025-03-05 08:47:20 +0100

    Add zsh plugin

--- flake.nix
+++ flake.nix
@@ -1,19 +1,65 @@
 {
   description = "My nix templates for programming";
 
-  outputs = _: {
-
-    templates = {
-      rust = {
-        path = ./templates/rust;
-        description = "My template for rust projects";
-      };
-
-      go = {
-        path = ./templates/go;
-        description = "My template for go projects";
-      };
-    };
-
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
+
+  outputs =
+    { nixpkgs, ... }:
+    {
+
+      templates = {
+        rust = {
+          path = ./templates/rust;
+          description = "My template for rust projects";
+        };
+
+        go = {
+          path = ./templates/go;
+          description = "My template for go projects";
+        };
+      };
+
+      defaultPackage =
+        let
+          supportedSystems = [
+            "x86_64-linux"
+            "aarch64-linux"
+          ];
+
+          forEachSystem = nixpkgs.lib.genAttrs supportedSystems;
+        in
+        forEachSystem (
+          system:
+          let
+            pkgs = import nixpkgs { inherit system; };
+          in
+          pkgs.stdenv.mkDerivation {
+            pname = "nix-start";
+            version = "0.1.0";
+
+            dontUnpack = true;
+
+            installPhase = ''
+              mkdir -p $out
+              cat > $out/nix-start.plugin.zsh <<EOF
+              function nix-start {
+                  nix flake init --template github:florentinl/nix-start#\$1
+              }
+
+              _nix_start() {
+                  local languages
+                  languages=(\$(nix flake show github:florentinl/nix-start --json | ${pkgs.lib.getExe pkgs.jq} -r '.templates | keys.[]'))
+
+                  _describe 'values' languages
+              }
+
+              compdef _nix_start nix-start
+              EOF
+            '';
+          }
+        );
+
+    };
 }
